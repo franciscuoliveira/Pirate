00f0a1552be5de5a02c7a248cdcf5512eb4741e9
JoanaPoAlmeida <58987530+JoanaPoAlmeida@users.noreply.github.com>
2021-11-27 20:53:45 +0000

    database iniciada

--- jogo/app.js
+++ jogo/app.js
@@ -1,5 +1,5 @@
 
-//iniciar servidor segundo o video
+//iniciar servidor
 const express = require("express");
 const app = express();
 
@@ -13,6 +13,24 @@ app.listen(5000, () => {
 } )
 
 
+//start my sql
+const mysql = require("mysql");
+
+const dbase = mysql.createConnection({
+  host:'localhost',
+  user: 'root',
+  password:'',
+  database:"pirate-treasure",
+});
+
+dbase.connect( (error) => {
+  if(error) {
+    console.log(error)
+  } else {
+    console.log("Database Connected!");
+  }
+});
+
 //cenas abaixo poderam ser alteradas
 /*
 const http = require('http');
@@ -21,8 +39,6 @@ const hostname = 'localhost';
 var bodyParser = require('body-parser')
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const mysql = require('mysql');
-
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
